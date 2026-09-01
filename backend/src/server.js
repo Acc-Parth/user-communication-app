52f8bd04929eb45d3f2a3a7368b2c434a0348dab
Acc-Parth <parth.s.dabholkar@accenture.com>
2026-09-01 20:48:20 +0530

    Production Deployment Code

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,27 @@ import express from "express"; // => es6 module import
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import msgRoutes from "./routes/messages.route.js";
+import path from "path"; //In-built in nodejs
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const __dirname = path.resolve(); // to get the current directory path
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", msgRoutes);
+
+// Deployment ready code
+if(Process.env.NODE_ENV === "production") {
+    //serve static files(frontend) to express server
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    //if any route other than api routes is hit, serve the index.html file
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
